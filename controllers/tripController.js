ded4ab48bc5507a37b7b4c8a2c615a922fc56964
Tim Mandel <timothy.mandel@gmail.com>
2019-02-10 14:59:11 -0600

    Trip post route made

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -4,7 +4,21 @@ const User = require('../models/user');
 const Trip = require('../models/trip')
 
 
-
+router.post('/', async(req, res) => {
+	try {
+		const newTrip = await Trip.create(req.body);
+		const foundUser = await User.findById(req.session._id);
+		foundUser.trips.push(newTrip);
+		await foundUser.save()
+		res.json({
+			status: 200,
+			data: foundUser
+		})
+	} catch(err) {
+		console.log(err);
+		res.send(err);
+	}
+})
 
 
 
