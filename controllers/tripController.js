dc519c0ceec090065e1b68537496aa64212a299e
Tim Mandel <timothy.mandel@gmail.com>
2019-02-11 17:20:49 -0600

    Yelp node added, and can get responses

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-const Trip = require('../models/trip')
+const Trip = require('../models/trip');
+const yelp = require('yelp-fusion');
 
 
 router.post('/', async(req, res) => {
@@ -22,6 +23,46 @@ router.post('/', async(req, res) => {
 		res.send(err);
 	}
 })
+
+
+router.get('/yelp/:id', async (req, res) => {
+	const foundTrip = await Trip.findById(req.params.id);
+	console.log(foundTrip, 'foundtrip');
+	const searchRequest = {
+		location: foundTrip.name,
+		attributes: 'hot_and_new'
+	};
+
+	const client = yelp.client('DXtElh5EDH3h6uff8IZFr_iQDaAgkwqNQAKYCHo2aj8MrD0gZ__8cdvU6Md2da_gz1asTph1FJ70oLk0UnugdW2iHb7r8c8DSGbPaQSDmNEiS52wDqVOwc31OIhdXHYx');
+
+	client.search(searchRequest).then(response => {
+		// const firstResult = response.jsonBody.businesses[0];
+		// const prettyJson = JSON.stringify(firstResult, null, 4);
+		console.log(response, ' responsesseses');
+		res.json({
+			status: 200,
+			data: response
+		})
+	}).catch(e => {
+		console.log(e);
+	});
+	// try{
+	// 	const yelpResponse = await fetch (`https://api.yelp.com/v3/businesses/search?location=${foundTrip.name}&attributes=hot_and_new`, {
+	// 		headers: {
+	// 			'Authorization': 'Bearer DXtElh5EDH3h6uff8IZFr_iQDaAgkwqNQAKYCHo2aj8MrD0gZ__8cdvU6Md2da_gz1asTph1FJ70oLk0UnugdW2iHb7r8c8DSGbPaQSDmNEiS52wDqVOwc31OIhdXHYx'
+	// 		}
+	// 	})
+	// 	console.log(yelpResponse, ' yelpresponse');
+	// 	res.json({
+	// 		status: 200,
+	// 		data: yelpResponse
+	// 	})
+	// } catch(err) {
+	// 	res.send(err)
+	// }
+})
+
+
 
 router.get('/:id', async(req, res) => {
 	try{ 
