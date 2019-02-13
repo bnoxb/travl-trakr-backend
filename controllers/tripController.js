b59735ab9baf7b7ecd9c0fe8ff92210e12287752
Tim Mandel <timothy.mandel@gmail.com>
2019-02-12 19:46:20 -0600

    PUT route for trips made

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -61,6 +61,21 @@ router.get('/:id', async(req, res) => {
 	}
 })
 
+router.put('/:id', async (req, res)=>{
+	try {
+		const updatedTrip = await Trip.findByIdAndUpdate(req.params.id, req.body, {new: true});
+		const foundUser = await User.findOne({'trips._id': req.params.id});
+		foundUser.trips.id(req.params.id).remove();
+		foundUser.trips.push(updatedTrip);
+		const data = await foundUser.save();
+		res.json({
+			status: 200,
+			data: updatedTrip
+		});
+	} catch(err) {
+		res.send(err)
+	}
+});
 
 router.delete('/:id', async (req, res) => {
 	try {
